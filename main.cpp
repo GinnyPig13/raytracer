44b3d15500227d653eb7460d99bc0684eeec3be2
GinnyPig13 <ginny.caynak@gmail.com>
2022-08-17 02:31:20 -0700

    Created basic ray functionality

--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 #include "vec3.h"
 #include "color.h"
+#include "ray.h"
+
+vec3 ray_color(const ray &r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    double t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+}
 
 int main(){
     // Image
 
-    const int image_width = 256;
-    const int image_height = 256;
+    const double aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
 
+    //Camera
+    double viewport_height = 2.0;
+    double viewport_width = aspect_ratio * viewport_height;
+    double focal_length = 1.0;
+
+    vec3 origin = vec3(0, 0, 0);
+    vec3 horizontal = vec3(viewport_width, 0, 0);
+    vec3 vertical = vec3(0, viewport_height, 0);
+    vec3 lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+    
     // Render
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
@@ -17,7 +35,10 @@ int main(){
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i)
         {
-            vec3 pixel_color = vec3(double(i)/(image_width-1), double(j)/(image_height-1), 0.25);
+            double u = double(i) / (image_width - 1);
+            double v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            vec3 pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
--- vec3.h
+++ vec3.h
@@ -13,22 +13,85 @@ class vec3 {
         vec3() : comp{0,0,0} {}
         vec3(double comp0, double comp1, double comp2) : comp{comp0, comp1, comp2} {}
 
-        double x() const 
-        { 
-            return comp[0];
+        double x() const {return comp[0];}
+        double y() const {return comp[1];}
+        double z() const {return comp[2];}
+
+      //vector operators  
+        vec3 operator-() const { return vec3(-comp[0], -comp[1], -comp[2]);}
+        double operator[](int i) const {return comp[i];}
+        double& operator[](int i) {return comp[i];}
+
+        vec3& operator+=(const vec3 &v) {
+            comp[0] += v.comp[0];
+            comp[1] += v.comp[1];
+            comp[2] += v.comp[2];
+            return *this;
+        }
+
+        vec3& operator*= (const double t) {
+            comp[0] *= t;
+            comp[1] *= t;
+            comp[2] *= t;
+            return *this;
+        }
+
+        vec3& operator/=(const double t) {
+            return *this *= 1/t;
+        }
+
+        double length() const {
+            return sqrt(length_squared());
         }
         
-        double y() const 
-        { 
-            return comp[1];
+        double length_squared() const{
+            return comp[0]*comp[0] + comp[1]*comp[1] + comp[2]*comp[2];
         }
-        
-        double z() const 
-        { 
-            return comp[2];
-        }
-        
-    //     vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]);}
+
 };
 
-using point3 = vec3;
+//utility functions
+
+inline std::ostream& operator<<(std::ostream &out, const vec3 &v) {
+    return out << v.comp[0] << ' ' << v.comp[1] << ' ' << v.comp[2];
+}
+
+inline vec3 operator+(const vec3 &u, const vec3 &v) {
+    return vec3(u.comp[0] + v.comp[0], u.comp[1] + v.comp[1], u.comp[2] + v.comp[2]);
+}
+
+inline vec3 operator-(const vec3 &u, const vec3 &v) {
+    return vec3(u.comp[0] - v.comp[0], u.comp[1] - v.comp[1], u.comp[2] - v.comp[2]);
+}
+
+inline vec3 operator*(const vec3 &u, const vec3 &v) {
+    return vec3(u.comp[0]*v.comp[0], u.comp[1]*v.comp[1], u.comp[2]*v.comp[2]);
+}
+
+inline vec3 operator*(double t, const vec3 &v) {
+    return vec3(t*v.comp[0], t*v.comp[1], t*v.comp[2]);
+}
+
+inline vec3 operator*(const vec3 &v, double t) {
+    return t * v;
+}
+
+inline vec3 operator/(const vec3 &v, double t) {
+    return (1/t) * v;
+}
+
+inline double dot(const vec3 &u, const vec3 &v) {
+    return u.comp[0] * v.comp[0]
+         + u.comp[1] * v.comp[1]
+         + u.comp[2] * v.comp[2];
+}
+
+inline vec3 cross(const vec3 &u, const vec3 &v) {
+    return vec3(u.comp[1] * v.comp[2] - u.comp[2] * v.comp[1],
+                u.comp[2] * v.comp[0] - u.comp[0] * v.comp[2],
+                u.comp[0] * v.comp[1] - u.comp[1] * v.comp[0]);
+}
+
+inline vec3 unit_vector(const vec3 &v) {
+    return v / v.length();
+}
