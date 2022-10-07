06cf710de14f32c01ab903f41ff93c84b25ba283
GinnyPig13 <ginny.caynak@gmail.com>
2022-10-06 20:24:23 -0700

    In which the glass material was finished

--- materials_bucket/dielectric.h
+++ materials_bucket/dielectric.h
@@ -6,21 +6,44 @@
 class dielectric : public material
 {
     public:
-        dielectric(double index_of_refraction) : ir(index_of_refraction) {}
+        dielectric(double index_of_refraction) : m_index_of_refraction(index_of_refraction) {}
 
         virtual bool scatter(const ray& r_in, const hit_record& rec_in, vec3& attenuation_out, ray& scattered_out) const override
         {
             attenuation_out = vec3(1.0, 1.0, 1.0);
-            double refraction_ratio = rec_in.is_front_face ? (1.0/ir) : ir;
+            double refraction_ratio = rec_in.is_front_face ? (1.0/m_index_of_refraction) : m_index_of_refraction;
 
             vec3 unit_direction = unit_vector(r_in.direction());
-            vec3 refracted = refract(unit_direction, rec_in.normal, refraction_ratio);
+            double cos_theta = fmin(dot(-unit_direction, rec_in.normal), 1.0);
+            double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
 
-            scattered_out = ray(rec_in.point, refracted);
+            bool it_cannot_refract = refraction_ratio * sin_theta > 1.0;
+            vec3 ray_direction;
+
+            if (it_cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double())
+            {
+                ray_direction = reflect(unit_direction, rec_in.normal);
+            }
+
+            else
+            {
+                ray_direction = refract(unit_direction, rec_in.normal, refraction_ratio);
+            }
+            
+            scattered_out = ray(rec_in.point, ray_direction);
             return true;
         }
 
 
     public:
-        double ir; //Index of Refraction
+        double m_index_of_refraction; 
+    
+    private:
+        static double reflectance(double cosine, double ref_idx)
+        {
+            //Use Schlick's approximation for reflectance
+            double R0 = (1-ref_idx) / (1+ref_idx);
+            R0 = R0*R0;
+            return R0 + (1-R0)*pow((1-cosine), 5);
+        }
 };
